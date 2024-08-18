e3ea92c38e9a9dc0d7340e844adc372e5144cc17
Arav-Mathur <aravmathur007@gmail.com>
2024-08-18 14:35:51 -0400

    added levels

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ pygame.display.set_caption("Fat Cat")
 
 clock = pygame.time.Clock()
 level_gen = LevelGenerator()
-level:int = 3
-cat : Player = Player(level_gen.get(str(level), "starting_pos"))
+level:int = 6
+cat : Player = Player(level_gen.get(str(level), "starting_pos"),(150,150))
 #blocks : list[Block] = [Block((0, 1000), (1920, 80), fileLoader.loadImage("Block.png")), Block((0, 0), (80, 1080), fileLoader.loadImage("Block.png")), Block((800, 0), (80, 1080), fileLoader.loadImage("Block.png"))]
 blocks : list[Block] = level_gen.generate_blocks(str(level))
 running = True
