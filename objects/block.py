540ea7408b142e2d3e7d17e44b1dfe018d0fede7
WaterThePotatoes <107214267+WaterThePotatoes@users.noreply.github.com>
2024-08-18 11:11:11 -0400

    Update block.py (water variable)

--- objects/block.py
+++ objects/block.py
@@ -1,19 +1,22 @@
 import pygame
 
 class Block():
-    def __init__(self, posIn : tuple[int, int], sizeIn : tuple[int, int], imageIn : pygame.image) -> None:
+    def __init__(self, posIn : tuple[int, int], sizeIn : tuple[int, int], imageIn : pygame.image, water = False) -> None:
         self.pos = posIn
 
         self.size = sizeIn
 
         self.image = pygame.transform.scale(imageIn, self.size)
 
+        self.water = water
+
     def get(self) -> dict[str, int]:
         return {
             "x" : self.pos[0],
             "y" : self.pos[1],
             "w" : self.size[0],
-            "h" : self.size[1]
+            "h" : self.size[1],
+            "water" : self.water
         }
 
     def draw(self, surfaceIn : pygame.Surface):
