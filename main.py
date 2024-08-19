feb2da1ac11ae9e1aa9204782675f6eb24d2fc81
Anvay Mathur <1mathuranv@hdsb.ca>
2024-08-18 20:06:40 -0400

    The scale works now and can be changed based on level

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ level: int = 1
 cat : Player = Player(level_gen.get(str(level), "starting_pos"),(100,100))
 #blocks : list[Block] = [Block((0, 1000), (1920, 80), fileLoader.loadImage("Block.png")), Block((0, 0), (80, 1080), fileLoader.loadImage("Block.png")), Block((800, 0), (80, 1080), fileLoader.loadImage("Block.png"))]
 blocks : list[Block] = level_gen.generate_object(str(level), Block, "blocks")
-
 #scales : list[Scale] = [Scale((100, 100), (50, 50), fileLoader.loadImage("Scale.png"))]
 fishes : list[Fish] = level_gen.generate_object(str(level), Fish, "fish")
-
+scales : list[Scale] = level_gen.generate_object(str(level), Scale, "scales")
 running = True
 
 while running:
@@ -58,6 +57,9 @@ while running:
 
     for fish in fishes:
         fish.draw(mainSurface)
+    
+    for scale in scales:
+        scale.draw(mainSurface)
 
     pygame.display.flip()
     clock.tick(60)
