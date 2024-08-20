164a0b36646dee9b74c9dd7b085045d05888ed5e
Anvay Mathur <1mathuranv@hdsb.ca>
2024-08-19 22:31:12 -0400

    lvl 10 hitboxes fixed

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ pygame.display.set_caption("Fat Cat")
 
 clock = pygame.time.Clock()
 level_gen = LevelGenerator()
-level: int = 5
+level: int = 10
 poof = Poof()
 cat : Player = Player(level_gen.get(str(level), "starting_pos"),(100,67), poof)
 blocks : list[Block] = level_gen.generate_object(str(level), Block, "blocks")
@@ -77,13 +77,13 @@ while running:
     # ------- DRAWING ------- #
     mainSurface.fill((255, 255, 255))
 
-    #for block in blocks:
-        #block.draw(mainSurface)
+   
 
     mainSurface.blit(background, (0, 0))
 
     cat.draw(mainSurface)
-
+    for block in blocks:
+        block.draw(mainSurface)
     if (level != 10):
         for fish in fishes:
             fish.draw(mainSurface)
