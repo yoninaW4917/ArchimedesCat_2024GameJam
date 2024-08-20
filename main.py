0a604b67d4f01e0cbabd041c4714e1cd022f2548
Arav-Mathur <aravmathur007@gmail.com>
2024-08-20 00:35:50 -0400

    oops

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ pygame.display.set_caption("Fat Cat")
 
 clock = pygame.time.Clock()
 level_gen = LevelGenerator()
-level: int = 10
+level: int = 1
 poof = Poof()
 cat : Player = Player(level_gen.get(str(level), "starting_pos"),(100,67), poof)
 blocks : list[Block] = level_gen.generate_object(str(level), Block, "blocks")
